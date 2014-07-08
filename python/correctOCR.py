65c659e3017da9b19c57f04e8a2be674d77d75a7
lawrence <lawrenceevalyn@gmail.com>
2014-07-08 00:21:39 -0700

    commented correctOCR.py

--- python/correctOCR.py
+++ python/correctOCR.py
@@ -1,4 +1,9 @@
 import re
+		#imports regular expressions
 book = open('badOCR.txt').read()
+		#makes book open and read badOCR.txt. what is book? is it... a function?
+# Need to insert something here that replaces "- " and "¬ " with nothing.
 new_book = re.sub('[^a-zA-Z\n\.]', ' ', book)
-open('fixedOCR.txt', 'w').write(new_book)
+		# makes new_book substitute nothingness for everything in book that isn't a letter
+open('fixedOCR.txt', 'w').write(new_book) 
+		# writes a new file with the results of new_book
