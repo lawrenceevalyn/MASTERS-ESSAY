f6261adc12f5ecacc06e8849d791a3cd9d393e41
lawrence <lawrenceevalyn@gmail.com>
2014-07-26 18:31:40 -0700

    correctOCR.py deletes disclaimer & newlines

--- python/correctOCR.py
+++ python/correctOCR.py
@@ -5,21 +5,29 @@ import re
 		# import regular expressions
 
 book = open('bad01.txt').read()
-		# read bad01.txt and hold it in memory as book
+		# read bad01.txt and hold it in the string book
 
 book = re.sub('- ', '', book)
 		# delete hyphen-space combos
-		# note that this one read book and executed changes in new_book
-		# future substitutions work just with new_book to build on each other
 
 book = re.sub('¬ ','', book)
 		# delete ¬-space combos
+
+book = re.sub('-\n','', book)
+		# delete hyphen-newline combos
 		
-book = re.sub('[^a-zA-Z\n\.]', ' ', book)
-		# substitute a space for everything that isn't a letter, new line, or period
+book = re.sub('\n',' ', book)
+		# replace newlines with space
+		
+book = re.sub('[^a-zA-Z\.]', ' ', book)
+		# substitute a space for everything that isn't a letter or period
+		# (this means it would delete newlines)
 
 book = re.sub('vv', 'w', book)
 		# change 'vv' to 'w'
-
+		
+book = re.sub('Disclaimer: This file is generated from OCR (optical character recognition), which is a technology that converts images of text into text. While the technology is good at deciphering legible text, there are limitations and some text may not have been extracted correctly.','', book)
+		# delete Corvey disclaimer text
+		
 open('fixed01.txt', 'w').write(book) 
 		# write fixed01.txt with book
